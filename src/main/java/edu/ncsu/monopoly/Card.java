882bf68afcedd9e4569b41fa205606ed44a52e36
jipitsaj <jipitsaj@U032857.adm.ehu.es>
2026-09-24 14:29:00 +0200

    upd

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//Añadimos comentario de nuevo
+//Añadimos comentario de nuevo hoy jueves 24
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
